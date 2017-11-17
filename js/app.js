19fcd3df05f6067d44d8fc7514f1d1c22338b1d7
lauraVelarde <penelope_rebeca_19@hotmail.com>
2017-11-17 11:35:37 -0500

    adding file html

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',function(e){
+    var colum = document.querySelectorAll('col');
+    colum.addEventListener('click',function(e){
+        colum.classList.add('purple');
+    })
+})
